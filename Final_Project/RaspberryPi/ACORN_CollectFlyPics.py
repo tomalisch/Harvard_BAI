bbc27f61858c4100b250cc0d8a930819815eadc8
tomalisch <alisch.tom@web.de>
2021-04-15 01:31:35 -0400

    2nd progress report
    without augmented images, else it'd be too big

--- Final_Project/RaspberryPi/ACORN_CollectFlyPics.py
+++ Final_Project/RaspberryPi/ACORN_CollectFlyPics.py
@@ -6,8 +6,11 @@ import cv2
 import os
 import time
 
+# Determine run length in seconds
+t_duration = 120*60
+
 # Check current time, add t seconds to it to determine end time
-t_end = time.time() + 60
+t_end = time.time() + t_duration
 
 # Set up camera resolution
 print("initializing camera")
@@ -21,7 +24,6 @@ camera.capture(rawCapture, format='rgb')
 image_old = rawCapture.array
 image_old_gray = cv2.cvtColor(image_old, cv2.COLOR_BGR2GRAY)
 
-
 # Start main loop for t seconds
 print("starting image acquisition")
 while time.time() < t_end:
@@ -31,18 +33,17 @@ while time.time() < t_end:
     camera.capture(rawCapture, format='rgb')
     image_new = rawCapture.array
     image_new_gray = cv2.cvtColor(image_new, cv2.COLOR_BGR2GRAY)
-
+    
     # Subtract new from old image as the difference image
-    image_diff = cv2.subtract(image_old_gray, image_new_gray)
-    # Gaussian blur the difference image to combat noise
-    image_diff_gblur = cv2.GaussianBlur(image_diff, (15,15), 0)
+    image_diff = cv2.absdiff(image_old_gray, image_new_gray)
+
     # Compute difference score as grand sum of gray pixel value of difference image
     diff_score = np.sum(image_diff)
-    if diff_score >= 50000000 or diff_score <= 10000000:
+    if diff_score >= 30000000 or diff_score <= 10000000:
         print("difference detected:", diff_score)
         # Save unblurred new image if significant difference detected
-        imagename = str(time.time()) + ".jpg"
-        imagepath = "/home/pi/Desktop/Images_Drosophila/"
+        imagename = str(time.time()) + "_" + str(diff_score) + ".jpg"
+        imagepath = "/media/pi/ACORN_THUMB/Images_Drosophila/"
         cv2.imwrite(os.path.join(imagepath, imagename), image_new)
         print("saved image as:", imagepath + imagename)
     
